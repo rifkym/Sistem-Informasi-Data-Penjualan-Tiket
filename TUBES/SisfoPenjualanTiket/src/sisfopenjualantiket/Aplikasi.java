2571c6a3a2dcca678b2aaaf48ec6019b84a78bc9
Herica Bunga M <hericabungamaharani@gmail.com>
2017-03-30 15:55:51 +0700

    Add files via upload

---
+++ TUBES/SisfoPenjualanTiket/src/sisfopenjualantiket/Aplikasi.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisfopenjualantiket;
+
+/**
+ *
+ * Herica Bunga M (1301154572)
+ */
+public class Aplikasi {
+    
+}
---
+++ TUBES/SisfoPenjualanTiket/src/sisfopenjualantiket/Driver.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sisfopenjualantiket;
+
+/** Nama Kelompok :
+ * Rifky Maulana (1301154180)
+ * Kurnia Wisuda Aji (1301150068)
+ * Herica Bunga Maharani (1301154572)
+ */
+public class Driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
