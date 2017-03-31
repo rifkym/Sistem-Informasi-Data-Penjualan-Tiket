d49219e2470c8ee056b7185732e51d84e2cc184d
rifkym <rratnawm@gmail.com>
2017-03-31 10:05:43 +0700

    Update Rute.java

--- TUBES/SisfoPenjualanTiket/src/sisfopenjualantiket/Rute.java
+++ TUBES/SisfoPenjualanTiket/src/sisfopenjualantiket/Rute.java
@@ -10,5 +10,25 @@ package sisfopenjualantiket;
  * Rifky Maulana (1301154180)
  */
 public class Rute {
+    private Stasiun stasiun1;
+    private Stasiun stasiun2;
+
+    public Stasiun getStasiun1() {
+        return stasiun1;
+    }
+
+    public void setStasiun1(Stasiun stasiun1) {
+        this.stasiun1 = stasiun1;
+    }
+
+    public Stasiun getStasiun2() {
+        return stasiun2;
+    }
+
+    public void setStasiun2(Stasiun stasiun2) {
+        this.stasiun2 = stasiun2;
+    }
+    
     
 }
+
