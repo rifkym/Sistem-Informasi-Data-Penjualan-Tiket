50a389e91ef7357ed825d8d8d228124cf55e0d8d
rifkym <rratnawm@gmail.com>
2017-03-31 10:06:23 +0700

    Update Stasiun.java

--- TUBES/SisfoPenjualanTiket/src/sisfopenjualantiket/Stasiun.java
+++ TUBES/SisfoPenjualanTiket/src/sisfopenjualantiket/Stasiun.java
@@ -9,6 +9,55 @@ package sisfopenjualantiket;
  *
  * Rifky Maulana (1301154180)
  */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author 0x17
+ */
 public class Stasiun {
+    private String nama;
+    private String alamat;
+    private String daerah_beroperasi;
+    private Kereta Kereta[];
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getDaerah_beroperasi() {
+        return daerah_beroperasi;
+    }
+
+    public void setDaerah_beroperasi(String daerah_beroperasi) {
+        this.daerah_beroperasi = daerah_beroperasi;
+    }
+
+    public Kereta[] getKereta() {
+        return Kereta;
+    }
+
+    public void setKereta(Kereta[] Kereta) {
+        this.Kereta = Kereta;
+    }
+    
     
 }
+
